0b80623e2573d364f086255ec63306dbad84d1fc
marcoslsalmeida <marcoslsalmeida@users.noreply.github.com>
2017-07-28 10:01:08 -0300

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wpbanco');
+define('DB_NAME', 'rdswordbanco');
 
 /** MySQL database username */
-define('DB_USER', 'wpuser');
+define('DB_USER', 'root');
 
 /** MySQL database password */
 define('DB_PASSWORD', '12345678');
 
 /** MySQL hostname */
-define('DB_HOST', 'wpbancodb.cmvwr3l9hulu.sa-east-1.rds.amazonaws.com');
+define('DB_HOST', 'rdsword1.cmvwr3l9hulu.sa-east-1.rds.amazonaws.com:3306');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
